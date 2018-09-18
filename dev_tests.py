b5b9df64f64eec461d9482ff1a0e73fe132b4fa7
Ben Cail <benjamin_cail@brown.edu>
2018-09-18 14:27:19 -0400

    add tests for various IDs

--- dev_tests.py
+++ dev_tests.py
@@ -5,6 +5,7 @@ from tests import SOLR_URL, BDR_PUBLIC, escape_solr_special_chars, get_solr_resu
 
 
 TEST_DATA = [
+        {'query': 'test:1', 'pid': 'test:1', 'lowest_ranking': 1},
         {'query': 'test:1125', 'pid': 'test:1125', 'lowest_ranking': 1},
         {'query': 'Cörper', 'pid': 'test:231377', 'lowest_ranking': 2},
         {'query': 'cörper', 'pid': 'test:231377', 'lowest_ranking': 2},
@@ -12,6 +13,9 @@ TEST_DATA = [
         {'query': 'corper', 'pid': 'test:231377', 'lowest_ranking': 2},
         {'query': 'sampt beygefügter', 'pid': 'test:231377', 'lowest_ranking': 1},
         {'query': 'sampt beygefugter', 'pid': 'test:231377', 'lowest_ranking': 1},
+        {'query': '111780829541025', 'pid': 'test:229775', 'lowest_ranking': 1}, #METS ID
+        {'query': 'askb013692', 'pid': 'test:224428', 'lowest_ranking': 1}, #MODS ID
+        {'query': 'Au-MP 1847 lf-1', 'pid': 'test:224428', 'lowest_ranking': 1}, #ASKB Call No.
     ]
 
 
@@ -24,7 +28,7 @@ class RelevancyTests(unittest.TestCase):
                 results = get_solr_results(query, rows=row['lowest_ranking'])
                 pids = [d['pid'] for d in results['response']['docs']]
                 pid = row['pid']
-                self.assertTrue(pid in pids, f'{pid} not in {pids} (query: "{query}")')
+                self.assertTrue(pid in pids, f'{pid} not in {pids} (query: "{query}")\n{results}')
 
 
 if __name__ == '__main__':
